333a71467d5d0456823eb301cc6a594c6fa390ba
Casey Johnson <johnson.casey1211@gmail.com>
2020-04-21 14:07:45 -0400

    removing a few lines of unnecessary console.log ccommands

--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from '../actions/filters';
 
